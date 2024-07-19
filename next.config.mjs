c24d3cb14d9b4f5b6469fa2164165704f4ecea9d
nohvenell2 <nohvenell2@gmail.com>
2024-07-20 07:49:34 +0900

    베포세팅 커밋

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/weather1' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/weather1' : '',
+    };
 
 export default nextConfig;
