3ff1a565ba5249403a31e82ddcbbce20a47d8d62
nohvenell2 <nohvenell2@gmail.com>
2024-07-20 07:49:34 +0900

    weather 베포 v1

--- util/weatherDB.js
+++
@@ -1,7 +0,0 @@
-import { connectDB } from "./database";
-async function weatherDB(){
-    const db = (await connectDB).db('weather');
-	const collection = await db.collection('weatherinfo');
-    return collection
-}
-export {weatherDB}
