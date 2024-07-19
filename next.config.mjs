2af09eb7bebfeb6dbab5c383457d7fd719ce2d18
nohvenell2 <nohvenell2@gmail.com>
2024-07-20 08:30:05 +0900

    미치겠다진짜

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/weather2' : '',
-    basePath: process.env.NODE_ENV === 'production' ? '/weather2' : '',
+    assetPrefix: '/weather2',
+    basePath: '/weather2'
     };
 
 export default nextConfig;
