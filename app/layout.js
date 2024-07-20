950771515078eb91fbf9dca908ae7210afdc6a1f
nohvenell2 <nohvenell2@gmail.com>
2024-07-20 10:29:26 +0900

    될거같은데

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Weather",
-  description: "Weather App",
+  description: "Weather App with git",
 };
 
 export default function RootLayout({ children }) {
