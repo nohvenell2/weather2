9036a6a1bfffcffbaaa7b101681f9f8fc1ce59bf
nohvenell2 <nohvenell2@gmail.com>
2024-07-20 12:05:25 +0900

    ceeff

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: "export",
     assetPrefix: '/weather2',
-    //basePath: '/weather2',
+    basePath: '/weather2',
     reactStrictMode: true
     };
 export default nextConfig;
