c6544402125623c26058a953246a1c5152a367fe
nohvenell2 <nohvenell2@gmail.com>
2024-07-26 04:44:10 +0900

    revalidate cash 변경 브랜치 생성 전

--- util/get-rawdata.js
+++ util/get-rawdata.js
@@ -2,6 +2,7 @@ export default async function get_rawdata(){
     try{
         const weatherdata = await fetch(process.env.WEATHERAPI_URL);
         const datajson = await weatherdata.json();
+        console.log('Raw Fetched.')
         return datajson;
     }catch(err){
         return null
