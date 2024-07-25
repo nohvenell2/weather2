4bd427e5348b8928dc536ae2438f987b8b0f8b3f
nohvenell2 <nohvenell2@gmail.com>
2024-07-26 02:13:21 +0900

    캐싱문제 해결

--- util/get-rawdata.js
+++ util/get-rawdata.js
@@ -1,6 +1,6 @@
 export default async function get_rawdata(){
     try{
-        const weatherdata = await fetch(process.env.WEATHERAPI_URL,{cache:'no-cache'});
+        const weatherdata = await fetch(process.env.WEATHERAPI_URL);
         const datajson = await weatherdata.json();
         return datajson;
     }catch(err){
